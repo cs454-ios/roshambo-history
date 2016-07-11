3cec5732a4bed0aa991adc47faaf6f22e89b6641
Jeanne <jpascua93@gmail.com>
2016-07-11 12:02:38 -0700

    added history view

---
+++ RoshamboWithHistory_incomplete/RockPaperScissors/HistoryViewController.swift
@@ -0,0 +1,55 @@
+//
+//  HistoryViewController.swift
+//  RockPaperScissors
+//
+//  Created by Jeanne Pascua on 7/11/16.
+//  Copyright © 2016 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var history = [RPSMatch]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return history.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("HistoryCell", forIndexPath: indexPath)
+        let match = self.history[indexPath.row]
+        
+        cell.textLabel!.text = victoryStatusDescription(match)
+        cell.detailTextLabel!.text = "\(match.p1) vs. \(match.p2)"
+        
+        return cell
+    }
+    
+    func victoryStatusDescription(match: RPSMatch) -> String {
+        if (match.p1 == match.p2) {
+            return "Tie."
+        } else if (match.p1.defeats(match.p2)) {
+            return "Win!"
+        } else {
+            return "Loss."
+        }
+    }
+
+    @IBAction func dismissHistory(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
